a24d7ba47c43ad741db373a1e85cb261220bd4bf
allmp13 <allmp13@gmail.com>
2017-02-11 21:26:42 +0100

    Ajout stop

--- VoletMySensors2/VoletMySensors2.ino
+++ VoletMySensors2/VoletMySensors2.ino
@@ -20,90 +20,120 @@ unsigned long depart;
 unsigned long fin;
 boolean enmarche;
 int positionvolet=0;
-/*String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;*/
 boolean sens; // 0 baisse 1 monte
 boolean stoppe = false;
 
 //MySensor gw;
 
 static int currentLevel = 0;  // Current dim level...
-MyMessage dimmerMsg(0, V_DIMMER);
-MyMessage lightMsg(0, V_LIGHT);
+MyMessage dimmerMsg(0,  V_PERCENTAGE);
+MyMessage btnstopMsg(1, V_STATUS);
 
-
-/***
- * Dimmable LED initialization method
- */
 void setup()  
 { 
-
   Serial.begin(115200);
   pinMode(VOLET_HAUT, OUTPUT);   //VOLET_HAUT 5
   pinMode(VOLET_STOP, OUTPUT);   //VOLET_STOP 6
   pinMode(VOLET_BAS, OUTPUT);  //VOLET_BAS 7
   pinMode(VOLET_CHANGE, OUTPUT); //VOLET_CHANGE 8
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+  while (!Serial) {
+  ; // wait for serial port to connect. Needed for Leonardo only
   }
-
-
-
-enmarche = false;
-fin = millis();
-//ecrireposvolet(0);
- Serial.println(__FILE__);
- Serial.println(__DATE__);
- Serial.println(__TIME__);
-//Serial.println(ip);
-//Serial.print(F("Position Initiale des Volets: "));
-//Serial.println(fin);
-  positionvolet=loadState(0);
-  //positionvolet=0;
-  Serial.println(positionvolet);
   
+  enmarche = false;
+  fin = millis();
+  //ecrireposvolet(0);
+  Serial.println("------------------SETUP----------------");
+  Serial.println(__FILE__);
+  Serial.println(__DATE__);
+  Serial.println(__TIME__);
+  Serial.println();
+  positionvolet=loadState(0);
+  Serial.print("Position Stockee = ");
+  Serial.println(positionvolet);
+  Serial.print("SN = ");
   Serial.println( SN ); 
-  //begin( incomingMessage );
-  request( 0, V_DIMMER );
+  Serial.print("Version = ");
+  Serial.println( SV ); 
+  send(dimmerMsg.set(positionvolet));
+
+  Serial.println("------------------FIN SETUP----------------");
 }
 
 void presentation()
 {
-  present( 0, S_DIMMER );
+  present( 0, S_DIMMER);
+  present( 1, S_BINARY);
   sendSketchInfo(SN, SV);
 }
 
 void loop() 
 {
   arretvolet();
-  //process();
 }
 
 
 
-void receive(const MyMessage &message) {
-  if (message.type == V_LIGHT || message.type == V_DIMMER) {
-    
-    //  Retrieve the power or dim level from the incoming request message
+void receive(const MyMessage &message) 
+{
+  int requestedLevel = atoi( message.data ); 
+  switch (message.sensor) 
+    {
+    case 0:
+      if ((requestedLevel >=0) and (requestedLevel <= 100))
+        {
+            Serial.print("Message Sensors0 = ");
+            Serial.println(message.data);
+            if (!enmarche) 
+             {
+               depart=millis();
+               fin=depart+activevolet(requestedLevel);
+             }
+        }
+      break;
+    case 1:
+      Serial.print("Message Sensors1 = ");
+      Serial.println(message.data);
+      if (requestedLevel==1)
+        {             
+           if (enmarche) 
+             {
+              stoppe=true;
+             }
+           send(btnstopMsg.set(0));
+        }
+        break;
+     }
 
-    int requestedLevel = atoi( message.data );
-    if (  requestedLevel == 999)
-    {
-       if (enmarche) 
-               {
-                stoppe=true;
-               }
-      }
-    else
-    {
-        if (!enmarche) 
-               {
-                 depart=millis();
-                 fin=depart+activevolet(requestedLevel);
-               }
-    }
-   
-    }
+  
+  /*if (message.type == V_LIGHT || message.type ==  V_PERCENTAGE) 
+    {   
+      Serial.print("Message Jeedom1 = ");  
+      Serial.println(message.data);
+      Serial.println(message.sensor);
+      Serial.println(message.type);
+      if (message.data=="STOP")
+        {             
+           if (enmarche) 
+             {
+              stoppe=true;
+             }
+        }
+       else
+        {
+          int requestedLevel = atoi( message.data ); 
+          if ((requestedLevel >=0) and (requestedLevel <= 100))
+            {
+                Serial.print("Message Jeedom2 = ");
+                Serial.println(requestedLevel);
+                if (!enmarche) 
+                 {
+                   depart=millis();
+                   fin=depart+activevolet(requestedLevel);
+                 }
+            }
+        } 
+    }*/
 }
 
 
@@ -149,18 +179,22 @@ return abs(delta)*260;
 
 void monte()
 {
+  
   digitalWrite(VOLET_HAUT, HIGH);   // set the LED on
   delay(100);              // wait for a second
   digitalWrite(VOLET_HAUT, LOW);    // set the LED off 
+  
   Serial.println(F("Monte"));
   sens=true;
 }
 
 void baisse()
 { 
+  
   digitalWrite(VOLET_BAS, HIGH);   // set the LED on
   delay(100);              // wait for a second
   digitalWrite(VOLET_BAS, LOW);    // set the LED off
+  
   Serial.println(F("Baisse"));
   sens=false;
 }
@@ -186,8 +220,8 @@ void arret()
   digitalWrite(VOLET_STOP, LOW);    // set the LED off 
   //ecrireposvolet(positionvolet);
   // Inform the gateway of the current DimmableLED's SwitchPower1 and LoadLevelStatus value...
-  send(lightMsg.set(positionvolet > 0 ? 1 : 0));
-  send(dimmerMsg.set(positionvolet) );
+  /*send(lightMsg.set(positionvolet > 0 ? 1 : 0));*/
+  send(dimmerMsg.set(positionvolet));
   saveState(0, positionvolet);
   Serial.println(F("Arret"));
   }
